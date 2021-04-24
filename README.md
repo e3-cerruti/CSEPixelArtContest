01d6f61ae739639ea162566e56092e231b299546
Stephen P. Cerruti <scerruti@e3civichigh.com>
2021-04-24 10:50:58 -0700

    Added an entry for the 16x16 bit category.

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 UCSD CSE is hosting a [Pixel Art Competition](https://pixel-art.goto.ucsd.edu/).
 
+# 32x32 Entry
 The Python script ```conway``` uses the CSE Pixel Art library to generate my contest entry.
 
 My entry has two parts.
@@ -10,7 +11,7 @@ The first element is a scrolling background drawn by hand in [PixilArt](https://
 You will understand why either it was a good thing I didn't go to art school or maybe I should have gone to at least some art school.
 
 The second part is a Coe Spaceship from Conway's Game of Life. I chose the Coe Spaceship simply because it would fit inside the required space. In choosing a
-spaceship I was forced to move the grid to remain in view, this drove my idea for a defender like display. The code includes the elements to read an RLE file
+spaceship I was forced to move the grid to remain in view, this drove my idea for a defender like display. The code includes the elements to read an RLE file,
 so you can try other Conway structures.
 
 The command line to generate my contest entry:
@@ -27,4 +28,13 @@ Credits:
   * The code for Conway's Game of Life was shamelessly lifted from [Geeks for Geeks](https://www.geeksforgeeks.org/conways-game-life-python-implementation/) and in turn that code came from [Mahesh Venkitachalam](https://github.com/electronut/pp/tree/master/conway) supporting his book __Python Playground__. The code is provided under the MIT License.
   * [RLE2Image](https://github.com/rtvu/rle2img) is provided by Ryan Vu also under MIT license, I did make a change to the file parsing that may relate to Python 3.
 
-Because the UCSD Pixel Art library is distributed without a license I don't know what license to add here. Obviously the vast majority of this code is not mine and is licensed under an MIT license. Please feel free to use my changes in any way you like (CC-0).
+The UCSD Pixel Art library is distributed without a license therefore I don't know what license to add here. Obviously the vast majority of this code is not mine and is licensed under an MIT license. Please feel free to use my changes in any way you like (CC-0).
+
+# 16x16 Entry
+
+How this entry works can be reversed engineered from the code, but the secrets are supplied via command line. I suspect that the code could be modified to suss out the secrets.
+
+| ![Large pixel art](puzzle_encoded_large.gif?raw=true) | ![Small pixel art](puzzle_encoded.gif?raw=true) |
+| :-------------------------: | :-------------------------: |
+| Large: scale 10 (320x320) | Small: scale 1 (32x32)    |
+
---
+++ puzzle.py
@@ -0,0 +1,122 @@
+import argparse
+
+import numpy
+
+from CSEPixelArt import load_img, save_anim, save_img
+from morse import encrypt
+
+BLUE = (0, 98, 155)
+GOLD = (255, 205, 0)
+BLACK = (0, 0, 0)
+ENCODED = '_encoded'
+LARGE = '_large'
+GIF = '.gif'
+
+
+def encode_message(input_image, message):
+    output_image = input_image.copy()
+
+    row = 0
+    column = 0
+    for code in map(ord, message.upper()):
+        if row >= len(input_image):
+            print('Warning: Message too long.')
+            return output_image
+
+        while input_image[row][column] != BLACK:
+            output_image[row][column] = GOLD
+            column = (column + 1) % len(input_image)
+            if column == 0:
+                row += 1
+            if row >= len(input_image):
+                print('Warning: Message too long.')
+                return output_image
+
+        green_offset = code // 3
+        blue_offset = code - green_offset
+
+        output_image[row][column] = tuple(map(lambda i, j: i - j, BLUE, (0, green_offset, blue_offset)))
+        column = (column + 1) % len(input_image)
+        if column == 0:
+            row += 1
+
+    for r in range(row, len(input_image)):
+        for c in range(column, len(input_image[row])):
+            output_image[r][c] = GOLD
+        column = 0
+
+    return output_image
+
+
+def encode_morse(image, message):
+    """
+    The length of a dot is 1 time unit.
+    A dash is 3 time units.
+    The space between symbols (dots and dashes) of the same letter is 1 time unit.
+    The space between letters is 3 time units.
+    The space between words is 7 time units.
+    """
+    def morse_timing(char):
+        timing = {'.': 1, '-': 3, ' ': 3, '/': 7}
+        return timing[char]
+
+    def dim(pixel):
+        return max(pixel - 25, 0)
+
+    morse = encrypt(message.upper())
+    sequence = list(map(morse_timing, morse))
+    if sum(sequence) + len(sequence) + 6 > 60:
+        print('Warning: morse message too long, truncated')
+
+    images = []
+    image_off = [[list(map(dim, pixel)) for pixel in column] for column in image]
+    for period in sequence:
+        for n in range(period):
+            images.append(image)
+        images.append(image_off)
+
+    for i in range(6):
+        images.append(image_off)
+
+    return images
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Runs Conway's Game of Life simulation.")
+
+    parser.add_argument('--input', dest='input', required=False)
+    parser.add_argument('--count', action='store_true', required=False)
+    parser.add_argument('--message', dest='message', required=False)
+    parser.add_argument('--morse', dest='morse', required=False)
+
+    args = parser.parse_args()
+
+    input_image = numpy.zeros((16, 16))
+    if args.input:
+        input_image = load_img(args.input + GIF)
+
+    if args.count and input_image:
+        pixels = [pixel for column in input_image for pixel in column if pixel == BLACK]
+        print(f'There are {len(pixels)} black pixels.')
+
+    output_image = None
+    output_images = None
+
+    if args.message:
+        output_image = encode_message(input_image, args.message)
+
+    if args.morse and output_image:
+        output_images = encode_morse(output_image, args.morse)
+
+    if output_image and not output_images:
+        save_img(output_image, args.input + ENCODED + GIF, scale=1)
+        save_img(output_image, args.input + ENCODED + LARGE + GIF, scale=10)
+
+    if output_images:
+        save_anim(output_images, args.input + ENCODED + GIF, scale=1)
+        save_anim(output_images, args.input + ENCODED + LARGE + GIF, scale=10)
+
+
+# call main
+if __name__ == '__main__':
+    main()
